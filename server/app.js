c0b53c86ed5903ee3d87ea6fdf7e904677f5c491
MendozaEmerson <emendozahi@unsa.edu.pe>
2025-01-18 20:06:39 -0500

    Se corroboro el funcionamiento correcto de a base e datos y el server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './routes.js';
+
 const app = express();
+app.use(express.json());
 
 routes(app);
+console.log("test");
 
 app.listen(3000, function () {
     console.log('App escucha en el puerto 3000');
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,11 @@
 import { response } from "express";
+import { getUser } from "./database.js";
 
 export default function (app) {
 
-    app.get('/', (require, response) => {
-        response.json({ "Mensaje": "Get raiz correcto" });
+    app.get('/users/:id', async (req, res) => {
+        const users = await getUser(req.params.id);
+        res.status(200).send(users);
     });
 
 
